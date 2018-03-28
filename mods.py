6712e099f1c7162f973065ac7efc5c9e9bd6651d
Sm0k3s <obaga4@gmail.com>
2018-03-28 11:23:31 +0300

    adds moderators class methods

---
+++ mods.py
@@ -0,0 +1,21 @@
+import datetime
+
+class Moderators():
+	"""a moderator can only edit their own comment"""
+	def __init__(self):
+		pass
+
+	def new_moderator(self, username, password):
+		self.username = username
+		self.password = password
+
+	def add_comment(self, author, comment):
+		self.author = author
+		self.comment = comment
+
+		
+
+	def delete_comment(self, comments)
+
+	def login(self, timestamp,):
+		self.timestamp = datetime
